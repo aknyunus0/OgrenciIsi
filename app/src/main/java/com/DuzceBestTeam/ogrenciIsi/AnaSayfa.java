3eb4f254cf238f88fecebdb3b92a4e17a4eabb43
Alperen <alperenyozcu@hotmail.com>
2021-03-11 01:20:02 +0300

    Giriş Ekranı Yeni tasarımı biraz çalışıldı bikaç buton falan ekledim

--- app/src/main/java/com/DuzceBestTeam/ogrenciIsi/AnaSayfa.java
+++ app/src/main/java/com/DuzceBestTeam/ogrenciIsi/AnaSayfa.java
@@ -1,6 +1,7 @@
 package com.DuzceBestTeam.ogrenciIsi;
 
 import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
