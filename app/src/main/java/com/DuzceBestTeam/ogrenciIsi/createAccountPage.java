2c95862bd93650eb3442d9a5f583d252fba1fd9d
Furkan <405furkancelik@gmail.com>
2021-03-11 18:51:57 +0300

    AndroidX yapıldı Material Design implement edildi

--- app/src/main/java/com/DuzceBestTeam/ogrenciIsi/createAccountPage.java
+++ app/src/main/java/com/DuzceBestTeam/ogrenciIsi/createAccountPage.java
@@ -1,6 +1,6 @@
 package com.DuzceBestTeam.ogrenciIsi;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class createAccountPage extends AppCompatActivity {
