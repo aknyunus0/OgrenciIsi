5d076710884c363a6daba1b054a33bc2360e591f
Yunus AKIN <yunusakin849@gmail.com>
2021-04-16 23:13:29 +0300

    anasayfa

---
+++ app/src/main/java/com/DuzceBestTeam/ogrenciIsi/newJobAdv.java
@@ -0,0 +1,14 @@
+package com.DuzceBestTeam.ogrenciIsi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newJobAdv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_job_adv);
+    }
+}
