a0af9c19d075ce2ffa7a7eaedb22dc689a392082
AytacPARLAR <aytacparlar@gmail.com>
2021-04-19 00:57:50 +0300

    Fragmentlar Eklendi

--- app/src/main/java/com/DuzceBestTeam/ogrenciIsi/AnaSayfa.java
+++ app/src/main/java/com/DuzceBestTeam/ogrenciIsi/AnaSayfa.java
@@ -2,20 +2,27 @@ package com.DuzceBestTeam.ogrenciIsi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AnaSayfa extends AppCompatActivity {
 
-
+    HomePage_Fragment HomePage = new HomePage_Fragment();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ana_sayfa);
-        
+        loadFragment(HomePage);
+        BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 public void AddJobAdv_onClick(View v){
@@ -23,4 +30,37 @@ public void AddJobAdv_onClick(View v){
 
 }
 
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.home:
+                    fragment = new HomePage_Fragment();
+                    break;
+                case R.id.Search:
+                    fragment = new Search_Fragment();
+                    break;
+                case R.id.Notification:
+                    fragment = new Notification_Fragment();
+                    break;
+                case R.id.Profile:
+                    fragment = new Profile_Fragment();
+                    break;
+            }
+            return loadFragment(fragment);
+        }
+    };
 }
---
+++ app/src/main/java/com/DuzceBestTeam/ogrenciIsi/Recycle_Item.java
@@ -0,0 +1,14 @@
+package com.DuzceBestTeam.ogrenciIsi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Recycle_Item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle_item);
+    }
+}
