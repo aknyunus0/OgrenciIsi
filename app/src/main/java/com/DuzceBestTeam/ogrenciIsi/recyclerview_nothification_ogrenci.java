ff8fc04eb29d94acaf4f059ac0def65411533db3
AytacPARLAR <aytacparlar@gmail.com>
2021-04-29 00:16:32 +0300

    Ogrenci bildirim tasarım eklendi

---
+++ app/src/main/java/com/DuzceBestTeam/ogrenciIsi/recyclerview_nothification_ogrenci.java
@@ -0,0 +1,14 @@
+package com.DuzceBestTeam.ogrenciIsi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class recyclerview_nothification_ogrenci extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recyclerview_nothification_ogrenci);
+    }
+}
